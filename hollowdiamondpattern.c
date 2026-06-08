482ed247bb25c1668fa3765fb96d6484c81c4f5b
sharvisaini <YOUR_GITHUB_EMAIL>
2026-06-08 17:22:45 +0530

    updated files

--- hollowdiamondpattern.c
+++ hollowdiamondpattern.c
@@ -3,13 +3,23 @@ int main () {
     int i,n,j,k;
     printf("enter your number: ");
     scanf("%d", &n);
-    for (i=1;i<=n;i++) {
-        
+    for (i=1;i<=n;i++) { 
+        for (k=0;k<(n-i);k++) {
+                printf(" ");
+            }       
         if (i==1) {
             printf("*");
-            for (k=0;k<(n-i);k++) {
-                printf(" ");
-            }
+            
+        } else {
+            printf("*");
+        }
+
+        for (k=i;k<(i-2);k++) {
+                printf("     ");
+            }       
+        if (i==1) {
+            printf("*");
+            
         } else {
             printf("*");
         }
